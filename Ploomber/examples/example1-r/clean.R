061b483587cc934ae2be9f3836d8a1a5e73e244f
Jacques Booysen <booysenjacques@gmail.com>
2023-07-20 09:46:44 +0200

    latest

--- Ploomber/examples/example1-r/clean.R
+++ Ploomber/examples/example1-r/clean.R
@@ -9,7 +9,7 @@ df = read.csv(upstream$raw$data)
 # the values are bigger than 2 times the standard deviations from the mean
 the_sd = sd(df$sepal_length)
 the_mean = mean(df$sepal_length)
-df = df[df$sepal_length < the_mean + 2 * the_sd, ]
+df = df[df$sepal_length < the_mean + 1.5 * the_sd, ]
 df
 
 write.csv(df, product$data)
